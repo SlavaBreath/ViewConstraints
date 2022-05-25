0d362463b7ceca360108127ccd03ecb565ffd42d
SlavaBreath <nagornyak.slava@gmail.com>
2022-05-25 10:12:19 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ViewConstraints
+
+A description of this package.
---
+++ Sources/ViewConstraints/ViewConstraints.swift
@@ -0,0 +1,6 @@
+public struct ViewConstraints {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
